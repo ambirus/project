65513f6845bd5aa4ea9fd7b4891340a0c585afc3
AmbientPC <andreyot@gmail.com>
2019-07-24 20:09:51 +0300

    Added new "HttpNotFoundException"

---
+++ src/exceptions/HttpNotFoundException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Project\exceptions;
+
+use Exception;
+
+/**
+ * Class HttpNotFoundException
+ * @package Project\exceptions
+ */
+class HttpNotFoundException extends Exception
+{
+}
