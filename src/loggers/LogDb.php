31b8039d10903c9d098b045f7a8cd3cac9cb8f29
rabramov <rabramov@fix.ru>
2019-12-05 15:26:45 +0300

    Added new type of log

--- src/loggers/LogDb.php
+++ src/loggers/LogDb.php
@@ -2,6 +2,7 @@
 
 namespace Project\loggers;
 
+use Project\db\Table;
 use Project\Logger;
 
 /**
@@ -11,11 +12,39 @@ use Project\Logger;
 class LogDb implements Logger
 {
     /**
+     * @var Log
+     */
+    private $logInstance;
+
+    public function __construct()
+    {
+        $this->logInstance = new Log();
+    }
+
+    /**
      * @param string $errorType
      * @param string $errorMessage
      */
     public function log(string $errorType, string $errorMessage)
     {
-        // TODO: Implement log() method.
+        $this->logInstance
+            ->create([
+                'type' => $errorType,
+                'message' => str_replace("\n", '', $errorMessage)
+            ])
+            ->execute();
     }
+}
+
+/**
+ * Class Log
+ * @package Project\loggers
+ */
+class Log extends Table
+{
+    /**
+     * @var string
+     */
+    protected $name = 'log';
+
 }
